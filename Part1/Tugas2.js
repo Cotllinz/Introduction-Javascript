de831d8ee2095f2b9a2d7b7ffee5b9912d71ec26
Galeh <63383858+Cotllinz@users.noreply.github.com>
2020-11-18 21:55:38 +0800

    Add files via upload

---
+++ Part1/Tugas2.js
@@ -0,0 +1,16 @@
+const mtk = 80;
+const bahasaIndonesia = 20;
+const bahasaInggris = 0;
+const ipa = 100;
+const rataNilai = mtk >= 0 && mtk <= 100 && bahasaIndonesia >= 0 && bahasaIndonesia <= 100 && bahasaInggris >= 0 && bahasaInggris <= 100 && ipa >= 0 && ipa <= 100 ? 
+                 typeof mtk === 'number' && typeof bahasaIndonesia === 'number' && typeof bahasaInggris === 'number' && typeof ipa === 'number' ?
+                 (mtk + bahasaIndonesia + bahasaInggris + ipa)/4 : "Nilai yang anda masukan bukan Number"
+                 : "Cek nilai kembali"
+console.log(rataNilai)
+const cekPeringkatNilai =   rataNilai >= 0 && rataNilai <= 59 ? `Rata - Rata = ${rataNilai} \nGrade E`
+                          : rataNilai >= 60 && rataNilai <= 69 ? `Rata - Rata = ${rataNilai} \nGrade D`
+                          : rataNilai >=70 && rataNilai <= 79 ? `Rata - Rata = ${rataNilai} \nGrade C` 
+                          : rataNilai >=80 && rataNilai <= 89 ? `Rata - Rata = ${rataNilai} \nGrade B`
+                          : rataNilai >=90 && rataNilai <= 100 ? `Rata - Rata = ${rataNilai} \nGrade A`
+                          : "Nilai Rata - Rata Tidak ada" 
+console.log(cekPeringkatNilai)
