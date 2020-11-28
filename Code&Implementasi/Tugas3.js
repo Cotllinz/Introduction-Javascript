9b753b39494c3c91bf8bb01c86ab645176607953
Galeh <63383858+Cotllinz@users.noreply.github.com>
2020-11-29 03:08:30 +0800

    Add files via upload

---
+++ Code&Implementasi/Tugas3.js
@@ -0,0 +1,16 @@
+const divideAndSort = (sortNumb) => {
+  if (typeof sortNumb === "number" && sortNumb) {
+    let removeZero = sortNumb.toString().split(0);
+    let result = "";
+    for (let i = 0; i < removeZero.length; i++) {
+      result += removeZero[i]
+        .split("")
+        .sort((a, b) => a - b)
+        .join("");
+    }
+    console.log(Number(result));
+  } else {
+    console.log("Input harus number dan terisi/tidak kosong");
+  }
+};
+divideAndSort(5956560159466056);
